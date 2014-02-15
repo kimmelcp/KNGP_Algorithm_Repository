a033c3063f014d2c97fefed8482fd67d0fb2d247
kimmelcp <c5bone@yahoo.com>
2014-02-15 17:53:54 -0500

    KNGP Files
    The needed files for the KNGP Algorithm.

---
+++ KNGPCode/KNGPAlgorithm.R
@@ -0,0 +1,160 @@
+#Title: KNGP Algorithm
+#Author: Chad Kimmel
+
+#Summary: The following is the code for the KNGP Algorithm.  The user must input 4 sources of input.  
+#Go to bottom of this script for an explanation of the inputs needed by the user and the main function call.  
+
+require(foreach)
+require(pROC)
+
+#The main KGNPAlgorithm Call
+KNGPAlgorithm <- function(LinkKnowledgeMatrix,NodeKnowledgeVector,NodeNamesVector,rootNodes)
+{
+  #Obtain the transition probability matrix. 
+  TransitionProbMatrix <- getTransitionProbMatrix(LinkKnowledgeMatrix,apply(LinkKnowledgeMatrix,1,sum))
+  rm(LinkKnowledgeMatrix); gc()
+  
+  #Obtain the root node vector index
+  rootNodeVectorIndex <- foreach(i=1:length(rootNodes),.combine=c) %do% {which(NodeNamesVector==rootNodes[i])}
+  
+  #List of f values to test.  
+  f_vector <- c(0,0.5,1,2,5,10,50,100,1000,10000)
+  
+  #Find the best_f
+  best_f <- find_best_f(TransitionProbMatrix,NodeKnowledgeVector,rootNodeVectorIndex,f_vector)
+  
+  #Compute the prior prob vector using best_f
+  priorProbVector <- getPriorProbVector(rootNodeVectorIndex,NodeKnowledgeVector,best_f)
+  
+  #Obtain the posterior prob vector.  
+  posteriorProbVector <- inference(TransitionProbMatrix,matrix(priorProbVector,nrow=length(priorProbVector),ncol=1))
+  
+  #Pass back data frame
+  result <- data.frame(cbind(name=NodeNamesVector,postProb=posteriorProbVector))
+  names(result) <- c("name","postProb")
+  result$name <- as.character(result$name)
+  result$postProb <- as.numeric(as.character(result$postProb))
+  result$name <- ifelse(result$name %in% rootNodes,paste0(result$name,"*"),result$name)
+  return(list(KNGPResult=result,best_f=best_f))
+  
+}
+
+#The find_best_f procedure.  
+find_best_f <- function(TransitionProbMatrix,NodeKnowledgeVector,rootNodeVectorIndex,f_vector)
+{
+  best_f <- NULL
+  best_AUC <- -100000
+  
+  nonRootNodes <- setdiff(1:dim(TransitionProbMatrix)[1],rootNodeVectorIndex) #Get the vector of all non-root nodes
+  
+  for (f in f_vector)
+  {
+    f_result <- foreach(i=1:length(rootNodeVectorIndex), .combine=rbind) %do%
+    {
+      rootIndex <- rootNodeVectorIndex[i] #The root index for the given loop
+      proteinsToRank <- c(rootIndex, sample(nonRootNodes,getSampleSize(length(nonRootNodes))) ) #Create the list of proteins to rank.
+      
+      newRootNodes <- setdiff(rootNodeVectorIndex,rootIndex) #The new root nodes minus the rootIndex
+      
+      priorProbVector <- getPriorProbVector(newRootNodes,NodeKnowledgeVector,f) #Compute the prior prob vector
+      posteriorProbVector <- inference(TransitionProbMatrix,matrix(priorProbVector,nrow=length(priorProbVector),ncol=1)) #Compute the posterior prob vector.
+      data.frame(cbind(score=posteriorProbVector[,1][proteinsToRank],actual=c(1,rep(0,length(proteinsToRank)-1))))
+    }
+    auc <- as.numeric(auc(f_result$actual,f_result$score))
+    if (auc>best_AUC) {best_AUC=auc;best_f=f}
+
+  }
+  
+  return(best_f)
+         
+}
+
+inference <- function(TransitionProbMatrix,priorProbMatrix)
+{
+  B = 0.5
+  threshold = 0.00001
+  delta = 1
+  Po_prev = matrix(rep(0,dim(priorProbMatrix)[1]),nrow=dim(priorProbMatrix)[1],ncol=1)
+  i = 1
+  
+  while (delta>threshold)
+  {
+    Po_curr <- ((1-B)*(TransitionProbMatrix %*% Po_prev)) + (B*priorProbMatrix)
+    delta <- abs(sum(Po_curr) - sum(Po_prev))
+    Po_prev <- Po_curr
+#     print(paste("Iteration",i,"Delta",delta))
+#     i <- i + 1
+  }
+  
+  return(Po_prev)
+}
+
+
+
+#Get the transition probability matrix
+getTransitionProbMatrix <- function(TransitionProbMatrix,sumMatrix)
+{
+  for (r in 1:dim(TransitionProbMatrix)[1])
+  {
+    for (c in 1:dim(TransitionProbMatrix)[1])
+    {
+      TransitionProbMatrix[r,c] <- TransitionProbMatrix[r,c]/sumMatrix[c]
+    }
+  }
+  return(TransitionProbMatrix)
+}
+
+getPriorProbVector <- function(rootNodes, NodeKnowledgeVector, f)
+{
+  newPriorValueVector <- foreach(s=1:length(NodeKnowledgeVector), .combine=c) %do% 
+  {
+    if (s %in% rootNodes) NodeKnowledgeVector[s]*f else
+      NodeKnowledgeVector[s]
+  }
+  return(newPriorValueVector/sum(newPriorValueVector))
+}
+
+#Get the sample size to pull from.
+getSampleSize <- function(size)
+{
+  if (size>=99) return(99) else
+    return(size)
+}
+
+# #Define initial paramete values.  The 4 input parameters are the followin:
+# # 1.  The n by n dimenstional link knowledge matrix
+# # 2.  The n dimensional vector of node knowledge values
+# # 3.  The n dimensional vector of node names
+# # 4.  The vector of node names which are root nodes.  
+# LinkKnowledgeMatrix <- matrix(data=c(1.0,0.3,0.3,0.1,0.3,0.3,1.0,0.6,0.2,0.3,0.3,0.6,1.0,0.1,0.4,0.1,0.2,0.1,1.0,0.2,0.3,0.3,0.4,0.2,1.0),nrow=5,ncol=5)
+# NodeKnowledgeVector <- c(20,40,20,20,40)
+# NodeNamesVector <- c("a","b","c","d","e")
+# rootNodes <- c("a","b")
+# 
+# #Read in values from input file.  
+# LinkKnowledgeCSV <- read.csv("C:\\PHDProject-GeneIdent\\Documents\\TechnicalPaper\\KNGPExampleInput\\LinkKnowledgeMatrix.csv",header=FALSE)
+# LinkKnowledgeMatrix <- as.matrix(LinkKnowledgeCSV)
+# 
+# NodeKnowledgeCSV <- read.csv("C:\\PHDProject-GeneIdent\\Documents\\TechnicalPaper\\KNGPExampleInput\\NodeKnowledgeVector.csv",header=FALSE)
+# NodeKnowledgeVector <- NodeKnowledgeCSV[,c(1)]
+# 
+# NodeNamesCSV <- read.csv("C:\\PHDProject-GeneIdent\\Documents\\TechnicalPaper\\KNGPExampleInput\\CandidateNodeSet.csv",header=FALSE)
+# NodeNamesVector <- as.character(NodeNamesCSV[,c(1)])
+# 
+# RootNodesCSV <- read.csv("C:\\PHDProject-GeneIdent\\Documents\\TechnicalPaper\\KNGPExampleInput\\RootNodeSet.csv",header=FALSE)
+# rootNodes <- as.character(RootNodesCSV[,c(1)])
+# # 
+##Dissertation Input
+# NodeNamesCSV <- read.csv("C:\\PHDProject-GeneIdent\\Documents\\TechnicalPaper\\DissertationInput\\NodeNameVector.csv",header=FALSE)
+# NodeNamesVector <- as.character(NodeNamesCSV[,c(1)])
+# 
+# NodeKnowledgeCSV <- read.csv("C:\\PHDProject-GeneIdent\\Documents\\TechnicalPaper\\DissertationInput\\NodeKnowledgeVector.csv",header=FALSE)
+# NodeKnowledgeVector <- NodeKnowledgeCSV[,c(1)]
+# 
+# RootNodesCSV <- read.csv("C:\\PHDProject-GeneIdent\\Documents\\TechnicalPaper\\DissertationInput\\SeedListCSVFormat\\AlzheimerSeedList.csv",header=FALSE)
+# rootNodes <- as.character(RootNodesCSV[,c(1)])
+# 
+# # # #Pass to KNGP procedure and produce output of posterior vaues ordered by Posterior Value.  
+# KNGPResult <- KNGPAlgorithm(LinkKnowledgeMatrix,NodeKnowledgeVector,NodeNamesVector,rootNodes)$KNGPResult
+# print(KNGPResult[order(KNGPResult$postProb,decreasing = TRUE),])
+
--- README.txt
+++ README.txt
@@ -1,1 +1,27 @@
-These are the files for the KNGP Algorithm.  
+These are the files to run the KNGP Algorithm. The KNGP code, written in R, 
+can be found at the following GitHub Gist URL:  https://gist.github.com/kimmelcp/9025360.
+Alternatively, there is also an online app for the KNGP algorithm at the 
+following URL: http://spark.rstudio.com/kimmelcp/KNGPApp/.  Note, the online app can not handle
+data-sets larger than 2 GB. If more space is needed, a higher-end machine should be sought.  
+
+The files in this repository consist of the following 4 CVS files. These files should be used as input 
+to the KNGP algorithm.  
+
+Link knowledge matrix: The link knowledge matrix is a n*n matrix where n is the number of 
+nodes in the knowledge network, and an entry in it represents the link weight between the 
+nodes specified by the row number and column number.  The link weight matrix in this repository represents the 
+similarity between all 17,691 proteins in UniProt.  The actual link weights are derived by combining known 
+protein interactions and predicted protein-protein interactions along with similarity values calculated
+from the Cellular Component Gene Ontology (GO).    
+
+Node knowledge vector: The node knowledge vector is a n dimensional vector where n is 
+the number of nodes in the knowledge network, and an entry in it represents the node weight 
+associated with the corresponding node.  The node weights in this repository represents the number of Gene Ontology
+terms associated with each of the unique 17,691 proteins.  
+
+Candidate node set: Set of n protein identifiers that correspond to the nodes in the node knowledge vector.  The
+proteins in this repository represents all 17,691 unique proteins in UniProt as of March 2011.   
+
+Root node set: A set of gene identifiers that is associated with the disease of interest and is a 
+subset of the candidate node set.  For this repostiry, the root nodes genes can be found in the folder 
+"SeedListCSVFormat".  There are a total of 19 diseases represented.  
